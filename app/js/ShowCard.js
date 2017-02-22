40215036082c75ea68a1a8154f37ddd8ee903314
olivecoil <adrian.s.tran@gmail.com>
2017-02-21 22:49:12 -0800

    Added ShowCard component which is called through search component

---
+++ app/js/ShowCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ShowCard = React.createClass({
+  render () {
+    const { poster, title, year, description } = this.props.show
+
+    return (
+      <div className='show-card'>
+        <img src={`/public/img/posters/${poster}`}
+          />
+        <div>
+          <h3>{title}</h3>
+          <h4>{year}</h4>
+          <h6>{description}</h6>
+        </div>
+      </div>
+    )
+  }
+})
+
+export default ShowCard
