f2374c8c7c01f4f1d07687a498495688717d662f
olivecoil <adrian.s.tran@gmail.com>
2017-02-24 22:16:26 -0800

    Added state and mounting functions to Detail component

--- app/js/Details.js
+++ app/js/Details.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './Header'
+import axios from 'axios'
 const { shape, string } = React.PropTypes
 
 const Details = React.createClass({
@@ -12,14 +13,33 @@ const Details = React.createClass({
       description: string
     })
   },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.props.show.imdbID}`)
+    .then((response) => {
+      this.setState({omdbData: response.data})
+    })
+    .catch((error) => console.error('axios error', error))
+  },
   render () {
     const { title, description, year, poster, trailer } = this.props.show
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='/public/img/loading.png' />
+    }
     return (
       <div className='details'>
         <Header />
-        <section> 
+        <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating} {/* declared above with condition statement */}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
